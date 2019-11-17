bbd1ae7e75289fbfae5182aaba2b497e90bd9723
Kuba Sukowski <kubasulek2@gmail.com>
2019-11-17 15:48:19 +0100

    project ready

---
+++ server/src/server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const express = require('express');
+const socketIo = require('socket.io');
+
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => console.log('Running on port ' + port));
+
+
+app.get('/express_backend', (req, res) => {
+	res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+});
+
+
+let count = 0;
+
+io.on('connection', (client) => {
+	client.emit('countUpdated')
+});
