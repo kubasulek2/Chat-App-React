e1d5890078e7fba66f0d0ce9983de88bac79211b
Kuba Sukowski <kubasulek2@gmail.com>
2019-11-18 12:14:18 +0100

    sockets with react hooks synchronized

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import openSocket from 'socket.io-client';
-const socketIOClient = openSocket('http://localhost:5000');
+import React, { useEffect, useState } from 'react';
+import { socket } from './socket';
 
-
-function App () {
-	const [ port ] = useState('http://localhost:5000');
-	const [counter, setCounter] = useState();
-	
-	const updateCounter = () => {
-	};
-	
+const App = () => {
+	const [ counter, setCounter ] = useState();
 	useEffect(() => {
-		const socket = socketIOClient.open(port);
-		socket.on('countUpdated', () => {
-			console.log('The Count has been updated');
+		socket.on('countUpdated', payload => {
+			setCounter(payload);
 		});
-		
-	},[]);
+	}, []);
+
+	const handleClick = () => {
+		socket.emit('increment');
+	};
+
+
+
 
 	return (
 		<React.Fragment>
-			<h1>I'm working</h1>
-			<button onClick={updateCounter}>Increment</button>
+			<h1>{counter}</h1>
+			<button onClick={handleClick}>Increment</button>
 		</React.Fragment>
 	);
-}
+
+};
 
 export default App;
--- server/src/server.js
+++ server/src/server.js
@@ -10,16 +10,16 @@ const io = socketIo(server);
 
 const port = process.env.PORT || 5000;
 
+
+
+
 server.listen(port, () => console.log('Running on port ' + port));
-
-
-app.get('/express_backend', (req, res) => {
-	res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
-});
-
 let count = 0;
-
 io.on('connection', (client) => {
-	console.log('object');
-	client.emit('countUpdated ', count);
+	console.log('Connected to Client');
+	client.emit('countUpdated', count);
+	client.on('increment',() => {
+		count++;
+		io.emit('countUpdated', count);
+	});
 });
