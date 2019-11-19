4784b62d08d5a8053d554fc70f48630f220fd113
Kuba Sukowski <kubasulek2@gmail.com>
2019-11-19 17:57:32 +0100

    frontend: displaying messages

--- server/src/server.js
+++ server/src/server.js
@@ -1,25 +1,38 @@
 const http = require('http');
 const express = require('express');
 const socketIo = require('socket.io');
+const Filter = require('bad-words');
 
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-
-
 const port = process.env.PORT || 5000;
 
-
-
-
 server.listen(port, () => console.log('Running on port ' + port));
-let count = 0;
+
 io.on('connection', (client) => {
-	console.log('Connected to Client');
-	client.emit('countUpdated', count);
-	client.on('increment',() => {
-		count++;
-		io.emit('countUpdated', count);
+	
+	client.emit('welcome', 'Welcome!');
+	client.broadcast.emit('welcome', 'New user has joined!');
+	
+	client.on('sendMessage', (message, cb) => {
+		const filter = new Filter();
+		
+		if(filter.isProfane(message)){	
+			return cb('Profanity is not allowed');
+		}
+
+		io.emit('message', message);
+		cb();
+	});
+
+	client.on('sendLocation', ({latitude, longitude}, cb) => {
+		io.emit('locationMessage', `https://google.com/maps?q=${ latitude },${ longitude }`);
+		cb('Location shared');
+	});
+
+	client.on('disconnect', () => {
+		io.emit('message', 'A user has left');
 	});
 });
