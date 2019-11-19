098598662e070b1ec76f7d7517e19aefdfb54fa9
Kuba Sukowski <kubasulek2@gmail.com>
2019-11-19 11:47:19 +0100

    frontend: manifest.json done, logos created, login in progress

--- client/src/components/UI/Login.js
+++ client/src/components/UI/Login.js
@@ -5,10 +5,10 @@ import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
+import InputAdornment from '@material-ui/core/InputAdornment';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 
-const useStyles = makeStyles(({palette, spacing}) => ({
+const useStyles = makeStyles(({ palette, spacing }) => ({
 	root: {
 		display: 'flex',
 		justifyContent: 'center',
@@ -18,21 +18,33 @@ const useStyles = makeStyles(({palette, spacing}) => ({
 	},
 	card: {
 		width: 345,
+		padding: `${ spacing(3) }px ${ spacing(1) }px`
 	},
 	inputContainer: {
 		marginBottom: spacing(2)
 	},
 	textField: {
 		width: '100%',
-		padding: 0
+		padding: 0,
+		'& .MuiInputBase-input': {
+			paddingTop: 14,
+			paddingBottom: 14,
+		},
+		'& .MuiInputBase-root':{
+			paddingLeft: 8
+		}
+	},
+	loginIcon: {
+		color: palette.primary.main
 	},
 	button: {
 		width: '100%',
 		borderRadius: 4,
 		fontSize: 20,
-		color: palette.grey[300],
+		color: palette.grey[ 300 ],
 		textTransform: 'capitalize'
-	}
+	},
+	border: { borderRadius: 4 },
 }));
 
 const Login = () => {
@@ -47,23 +59,29 @@ const Login = () => {
 				<CardContent>
 					<form>
 						<div className={classes.inputContainer}>
-							<Grid container alignItems="center">
-								<Grid item>
-									<AccountCircle />
-								</Grid>
-								<Grid item>
-									<TextField
-										id="filled-basic"
-										className={classes.textField}
-										label="User name"
-										variant="outlined"
-									/>
-								</Grid>
-							</Grid>
+							<TextField
+								id="filled-basic"
+								className={classes.textField}
+								placeholder="User name"
+								variant="outlined"
+								InputProps={{
+									startAdornment: (
+										<InputAdornment
+											position="start"
+										>
+											<AccountCircle
+												className={classes.loginIcon}
+												fontSize='large'
+											/>
+										</InputAdornment>
+									),
+									classes: { root: classes.border }
+								}}
+							/>
 						</div>
-						<Button 
-							variant='contained' 
-							color='primary'
+						<Button
+							variant='contained'
+							color='secondary'
 							className={classes.button}
 						>
 							continue
