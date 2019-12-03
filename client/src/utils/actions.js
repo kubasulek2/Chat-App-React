75154d468f0d3a0f3a3bead7a0ec8d12f3f48ee9
Kuba Sukowski <kubasulek2@gmail.com>
2019-12-03 11:38:59 +0100

    frontend: implemented immer for state updating, bug: can't close private chat without errors

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -1,62 +1,44 @@
-const joinRoom = (chat, room) => {
-	const chats = { ...chat.chats };
+import produce from 'immer';
 
-	delete chats[chat.room];
+const joinRoom = (chat, room) => produce(chat, draft => {
+	delete draft.chats[chat.room];
+	draft.chats[room] = { id: room, messages: [], unread: false };
+	draft.activeChat = room;
+	draft.room = room;
+});
 
-	chats[room] = { id: room, messages: [], unread: false };
 
-	return {
-		...chat,
-		activeChat: room,
-		chats: chats,
-		room
-	};
-};
+const setActive = (chat, active) => produce(chat, draft => {
+	draft.chats[active].unread = false;
+	draft.activeChat = active;
+});
 
-const setActive = (chat, active) => {
-	const chats = { ...chat.chats };
-	const activeChat = { ...chats[active] };
-	
-	activeChat.unread = false;
-	chats[active] = activeChat;
-
-	return { ...chat, chats, activeChat: active };
-};
 
 const addMessage = (chat, message) => {
-	const chats = { ...chat.chats };
 	const channel = message.privy ? message.sender : chat.room;
-	const messages = chats[channel].messages;
 
-	chats[channel].messages = [...messages, message];
-	chats[channel].unread = channel !== chat.activeChat;
-
-	return { ...chat, chats };
+	return produce(chat, draft => {
+		draft.chats[channel].messages.push(message);
+		draft.chats[channel].unread = channel !== chat.activeChat;
+	});
 };
 
-const setPrivate = (chat, id, userName) => {
-	const chats = { ...chat.chats };
+const setPrivate = (chat, id, userName) => produce(chat, draft => {
+	draft.chats[userName] = { id, messages: [] };
+});
 
-	chats[userName] = { id, messages: [] };
-
-	return { ...chat, chats };
-};
 
 const block = (chat, id, userName) => {
 	const chats = { ...chat.chats };
 	//chats[userName] = { id, messages: [] };
-	
-	return { ...chat, chats };
-};
-
-const close = (chat, chatName) => {
-	let chats = { ...chat.chats };
-	delete chats[chatName];
-	 
-	
 
 	return { ...chat, chats };
 };
+
+const close = (chat, chatName) => produce(chat, draft => {
+	draft.activeChat = chat.activeChat === chatName ? chat.room : chat.activeChat;
+	draft.chats[chatName].messages = [];
+});
 
 
 
